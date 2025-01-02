f83336a69a5c9e62e6cffaa80da09838706c9a8e
GEUMDOHYEON <dohyeon0518@gmail.com>
2025-01-02 20:38:41 +0900

    feat : Member Test Code

---
+++ src/test/java/org/team/defee/SimpleTest.java
@@ -0,0 +1,14 @@
+package org.team.defee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    void addition(){
+        System.out.println("addition");
+        assertEquals(2 + 3, 5);
+    }
+}
